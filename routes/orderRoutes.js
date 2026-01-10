b12876047cf567fda7fd89af1b5aaae0cb5d509b
Olakareem <olakareemomobolarinwa@gmail.com>
2026-01-10 14:08:13 +0100

    Update

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -67,7 +67,7 @@ router.post(
         payment_method_types: ['card'],
         line_items: lineItems,
         mode: 'payment',
-        success_url: `${process.env.CLIENT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+        success_url: `${process.env.CLIENT_URL}/pages/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.CLIENT_URL}/`,
         metadata: {
           mobileNumber,
